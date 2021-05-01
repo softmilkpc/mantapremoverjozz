a6d3aebaadf18606e897a51ea9c8413610b0807c
softmilkpc <70849324+softmilkpc@users.noreply.github.com>
2021-05-02 01:59:01 +0700

    Update commands.py

--- commands/commands.py
+++ commands/commands.py
@@ -19,10 +19,10 @@ from config import Config
 #Inline Keyboard Button
 keyboard = [
 [
- InlineKeyboardButton("Source Code", url=Config.SOURCE)
+ InlineKeyboardButton("Join Channel", url=Config.SOURCE)
 ],
 [
- InlineKeyboardButton("How To Create A Bot Like Me",url="https://youtu.be/swg6un2N4Fk")
+ InlineKeyboardButton("Youtube Gw",url="https://youtube.com/c/softmilk")
 ]
 ]
 
